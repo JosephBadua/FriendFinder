ccafd291c8c33431d5f1d8148e3400acb9b61a31
JosephBadua <josephbadua1006@gmail.com>
2019-10-25 19:28:05 -0700

    got all the links to work. Having a console issue with the 'require' annd 'exports' but everything functions as intended

--- server.js
+++ server.js
@@ -2,18 +2,20 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-var routes = require("./app/routing/htmlRoutes")
-var api = require("./app/routing/apiRoutes")
-var need = api.api;
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.static(path.join(__dirname, "./app/public/home.html")));
+app.use(express.static(path.join("./app")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var routes = require("./app/routing/htmlRoutes")
+var api = require("./app/routing/apiRoutes")
+var need = api.api;
+
 // Routes
 app.get("/", function(req, res) {
   res.sendFile(routes.home)
