2fdb88a5bacef6809234b5254aff46ee51790d7b
JosephBadua <josephbadua1006@gmail.com>
2019-10-27 15:52:16 -0700

    was able to have the user score match the closest score from the database scores, having trouble findin out how to match that closest score with the name of the friend it came with

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,10 @@
+
+var names = [];
+var endScores = [];
 $(document).ready(function(){
     $("#submit").on("click", function(event) {
         event.preventDefault();
-            var userData = {
+            let userData = {
               name: $("#name").val(),
               photo: $("#photo").val(),
               scores: [
@@ -17,16 +20,44 @@ $(document).ready(function(){
                 $("#q10").val()
               ]
             };
-            var arrayOfStrings = (userData.scores);
-            var arrayofNumber = arrayOfStrings.map(Number);
+            let arrayOfStrings = (userData.scores);
+            let arrayofNumber = arrayOfStrings.map(Number);
             const reducer = (accumulator, currentValue) => accumulator + currentValue;
-            var finalValue = arrayofNumber.reduce(reducer)
-            console.log(finalValue);
+            let finalValue = arrayofNumber.reduce(reducer)
+            console.log("End score is " + finalValue)
             // AJAX post the data to the friends API.
             $.post("/api/friends", userData).then(function() {
-              console.log("done");
-              console.log(userData);
               });
+             $.get("/api/friends").then(function(data) {
+              let pastFriends = data
+              for (let i = 0; i < pastFriends.length; i++){
+                names.push(pastFriends[i].name)
+                let scoreStrings = pastFriends[i].scores
+                console.log(scoreStrings)
+                let scoreNumbers = scoreStrings.map(Number);
+                let scoreValues = scoreNumbers.reduce(reducer);
+                console.log(scoreValues);
+                endScores.push(scoreValues);
+              }
+              endScores.splice(-1,1);
+              console.log(endScores);
+              console.log(names);
+              console.log((closest(endScores,finalValue)));
+              });
+              function closest(array,num){
+                  var i=0;
+                  var minDiff=1000;
+                  var ans;
+                  for(i in array){
+                      var m=Math.abs(num-array[i]);
+                      if(m<minDiff){ 
+                              minDiff=m; 
+                              ans=array[i]; 
+                          }
+                    }
+                  return ans;
+              }
+              console.log((closest(endScores,finalValue)));
 
     });
 });
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,37 @@
-var friends = [ { name: 'j',
-photo: 'j',
-scores: [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' ] }]
+var friends = [ 
+    { 
+    name: 'j',
+    photo: 'j',
+    scores: [ 
+    '1', 
+    '1', 
+    '1', 
+    '1', 
+    '5', 
+    '1', 
+    '1', 
+    '1', 
+    '1', 
+    '1' 
+        ] 
+    },
+    { 
+    name: 'jos',
+    photo: 'jos',
+    scores: [ 
+    '4', 
+    '3', 
+    '2', 
+    '4', 
+    '5', 
+    '1', 
+    '5', 
+    '4', 
+    '1', 
+    '2' 
+        ] 
+    }
+]
   
 var api = JSON.stringify(friends);
 
