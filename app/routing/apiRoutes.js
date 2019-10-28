2190924539086784ba0041dc62e3334ebe73cb8f
JosephBadua <josephbadua1006@gmail.com>
2019-10-28 12:46:48 -0700

    added two more 'friends' and the github repo link

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -30,6 +30,38 @@ var friends = [
     '2', 
     '2' 
         ] 
+    },
+    { 
+    name: 'Batman',
+    photo: 'https://upload.wikimedia.org/wikipedia/en/8/87/Batman_DC_Comics.png',
+    scores: [ 
+    '1', 
+    '1', 
+    '1', 
+    '1', 
+    '1', 
+    '1', 
+    '1', 
+    '1', 
+    '1', 
+    '2' 
+        ] 
+    },
+    { 
+    name: 'Sauron',
+    photo: 'https://vignette.wikia.nocookie.net/middleearthshadowofmordor7723/images/6/64/SauronTheBrightLordMissionImage.jpg/revision/latest/scale-to-width-down/1000?cb=20180326151346',
+    scores: [ 
+    '3', 
+    '3', 
+    '3', 
+    '3', 
+    '3', 
+    '3', 
+    '3', 
+    '3', 
+    '3', 
+    '2' 
+        ] 
     }
 ]
   
@@ -37,3 +69,4 @@ var api = JSON.stringify(friends);
 
 exports.friends = friends
 exports.api = api
+
